6f4867ceaaeeb18db1a23dc21abdc8ae106c7875
xperalta <xabi@Xabiers-MacBook-Pro.local>
2021-01-14 23:38:41 -0500

    added daytime and night

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,9 +1,21 @@
+let dt;
+let nt;
+
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	createCanvas(500,288); // make an HTML canvas element width x height pixels
+
+}
+function preload(){
+	dt= loadImage('img/daytime.jpg');
 }
 
 function draw() {
-	background(225);
+	x= hour();
+	if(x > 6 && x < 18){
+		background(dt);
+	}else{
+		background(225);
+	}
 	textSize(32);
 	fill(180);
 	text(hour(), 10, 30);
@@ -11,4 +23,8 @@ function draw() {
 	text(minute(), 10, 60);
 	fill(0);
 	text(second(), 10, 90);
+
 }
+
+//text() output the string at the specified postion
+//fill() fills all shapes drawn after with the specified color 
